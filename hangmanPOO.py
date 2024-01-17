d873bfe30550ea733fc0d2730ae3e825c9a66eec
rodrigofdl <rodrigo.fdlira@gmail.com>
2024-01-16 22:54:17 -0300

    Eight commit - I added the method to choice random word and display the puppet

--- hangmanPOO.py
+++ hangmanPOO.py
@@ -64,6 +64,10 @@ O   |
      |
 =========''']
 
+# Método para gerar uma palavra aleatória
+def gerar_palavra_aleatoria():
+    palavra_aleatoria = random.choice(['programacao', 'computador', 'python', 'sql', 'google'])
+    return palavra_aleatoria
 
 # Classe
 class Hangman:
@@ -78,8 +82,8 @@ class Hangman:
 # Método para começar o jogo
     def jogar(self):
         while self.tentativas_erradas < 6 and '_' in self.palavra_descoberta:
-            exibir_estado()
-            letra = solicitar_letra()
+            self.exibir_estado()
+            letra = self.solicitar_letra()
 
             if letra in self.letras_tentadas:
                 print("Você já tentou esta letra. Tente outra")
@@ -92,11 +96,12 @@ class Hangman:
             else:
                 self.tentativas_erradas += 1
 
-        exibir_resultado()
+        self.exibir_resultado()
 
 # Método para exibir o exibir o estado do jogo
     def exibir_estado(self):
-        print("Palavra: " + " ".join(self.palavra_descoberta))
+        print(board[self.tentativas_erradas])
+        print("\nPalavra: " + " ".join(self.palavra_descoberta))
         print("Letras tentadas: " + ", ".join(self.letras_tentadas))
         print("Tentativas restantes " + str(6 - self.tentativas_erradas))
 
@@ -113,7 +118,7 @@ class Hangman:
     def atualizar_palavra_descoberta(self, letra):
         for i, caractere in enumerate(self.palavra_secreta):
             if caractere == letra:
-                self.palavra_descoberta[i] = letra
+                self.palavra_descoberta[i] = letra.lower()
 
 # Método para incrementar tentativas erradas
     def incrementar_tentativas_erradas(self):
@@ -125,3 +130,7 @@ class Hangman:
             print("Parábens! Você descobriu a palavra: " + "".join(self.palavra_descoberta))
         else:
             print("Você perdeu! A palavra era: " + self.palavra_secreta)
+
+jogo = Hangman(gerar_palavra_aleatoria())
+
+jogo.jogar()
